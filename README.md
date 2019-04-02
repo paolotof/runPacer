b0802e6bea34a04c5143be00fa1334d47ba28e42
paolotof <varati@hotmail.com>
2019-04-02 15:56:55 +0200

    updated script and readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # runPacer
 Bash script containing sox commands to add a 180BPM beat to soundfiles 
 
+# requires
+SoX with mp3 extension and id3v2 (or comment out lines 44 and 45)
+
 # set permissions and make executable
 chmod u+x runPacer.sh
 
@@ -8,5 +11,4 @@ chmod u+x runPacer.sh
 ./runPacer.sh
 
 # more information
-Can be found at this link
-[Web interface for round 1](https:// "round 1")
+Can be found at [this](https:// "construction of the runPacer") link
---
+++ runPacer.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# ------
+#
+# Create an array with all files in current directory and loop through them. 
+# If file is mp3 then continue to next file
+# Convert to mp3 if opus or ogg file format. Then convert to mono if file is 
+# stereo, synthesis a 180 BPM metronome beat made of a 500 Hz carrier sinusoid 
+# modulated in amplitude by a 3 Hz square wave, merge the sound file and 
+# metronome beat, remove unnecessary files and add id3v2 tag to ease selection
+#
+# run as:
+# 
+# ./runPacer.sh
+#
+# ------
+
+myfiles=(*.*)
+counter=1
+echo ${iWord}
+for iWord in "${myfiles[@]}"; do
+	if (( `expr "${iWord}" : '.*\.mp3'` > 0 )); then
+		echo "skipping " "${iWord}";
+		continue
+	fi
+	if (( `expr "${iWord}" : '.*\.ogg'` > 0 )); then 
+		echo "convert ogg to mp3";
+		fileName="${iWord%.ogg}.mp3";
+		sox "${iWord}" "$fileName";
+	fi
+	if (( `expr "${iWord}" : '.*\.opus'` > 0 )); then 
+		echo "convert opus to mp3";
+		fileName="${iWord%.opus}.mp3";
+		opusdec --force-wav "${iWord}" - | sox - "$fileName"
+	fi
+	echo ${iWord:0:`expr "$iWord" : '.*\.'`}"mp3"
+	sox "$fileName" "file1.mp3" remix 1
+	sox -n -r `soxi -r file1.mp3` "output.mp3" synth `soxi -d file1.mp3` sine 500 vol 0.5 synth `soxi -d file1.mp3` square amod 3
+	newFile=${fileName%.mp3}_beat.mp3
+	sox -m "file1.mp3" "output.mp3" "$newFile"
+	rm "$fileName";
+	rm "output.mp3";
+	rm "file1.mp3";
+	id3v2 --TALB "Running cadence" "$newFile";
+	id3v2 --TRCK $counter $newFile;
+	let counter=counter+1
+	echo "${iWord}" "done"
+done
